f37dca8a6b2d5e6e9148b325c466ffbc14f1996a
Anton <toxasava@icloud.com>
2019-05-05 01:42:27 -0400

    Updates

---
+++ precache-manifest.eb138ed8de974a28589b45de3fb7ec35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c4aaba2cc5aa2a869170e5579c0530",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "62ea5de0649d56d553ca",
+    "url": "/jamming/static/css/main.b69eb867.chunk.css"
+  },
+  {
+    "revision": "42ba4bdd89781df36a06",
+    "url": "/jamming/static/js/2.d9e2c9fc.chunk.js"
+  },
+  {
+    "revision": "62ea5de0649d56d553ca",
+    "url": "/jamming/static/js/main.b39bf808.chunk.js"
+  },
+  {
+    "revision": "c3d7dfc925f613b07d70",
+    "url": "/jamming/static/js/runtime~main.6e9a7a58.js"
+  },
+  {
+    "revision": "6f3f1d018173a7e6b64153c8407c1ef6",
+    "url": "/jamming/static/media/Spotify.6f3f1d01.png"
+  }
+]);
