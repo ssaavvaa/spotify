3a10ad634ff0aacd1b100e43bdb3a2ea30a07a27
Anton <toxasava@icloud.com>
2019-05-21 04:58:57 -0400

    Updates

---
+++ precache-manifest.3128b32f9bfbaf53635d2c481a022c41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e1002910d44f6dcf3553b676ef13e4",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "2dd2828da68e43abfb6f",
+    "url": "/jamming/static/css/main.2046bb5e.chunk.css"
+  },
+  {
+    "revision": "60150affe2847906a706",
+    "url": "/jamming/static/js/2.65de5ddf.chunk.js"
+  },
+  {
+    "revision": "2dd2828da68e43abfb6f",
+    "url": "/jamming/static/js/main.235f143f.chunk.js"
+  },
+  {
+    "revision": "c3d7dfc925f613b07d70",
+    "url": "/jamming/static/js/runtime~main.6e9a7a58.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jamming/precache-manifest.a436933f9a4d07b226883d85ec74ec08.js"
+  "/jamming/precache-manifest.3128b32f9bfbaf53635d2c481a022c41.js"
 );
 
 self.addEventListener('message', (event) => {
